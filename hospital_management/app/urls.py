80aa448ff983fdf0d4471c3bf8832e64f77662de
Anush Kar <anushk04@gmail.com>
2023-05-17 10:11:33 +0530

    new beginning

--- hospital_management/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.homepage),
-    path("login/", views.login),
-    path("signup/", views.signup),
-    path("logout/", views.logout)
-]
