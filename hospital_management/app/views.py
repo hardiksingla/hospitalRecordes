7db7366affac5cc3c3436dbaca7feb0882716649
hardiksingla <07hardiksingla@gmail.com>
2023-05-19 13:16:11 +0530

    starting with django

---
+++ hospital_management/app/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import pno
+
+
+# Create your views here.
+def homepage(request):
+    if request.user.is_authenticated:
+        return render(request, "index.html")
+    return redirect("/login")
+
+def login(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username,password)
+        
+        user = auth.authenticate(username = username, password = password)
+        print(user)
+        if user is not None:
+             auth.login(request, user)
+             messages.info(request, f"Welcome, {user.username}")
+             return redirect("/")
+        elif User.objects.filter(username = username).exists():
+            messages.info(request, "Password does not match")
+            return redirect("/login")
+        else:
+            messages.info(request, "Invalid Username")
+            return redirect("/login")
+
+    else:
+        return render(request, "login.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        repass = request.POST['repass']
+        email = request.POST['email']
+        number = request.POST['pno']
+        if User.objects.filter(username = username).exists():
+            messages.info(request, "Username exists")
+            return redirect("/signup")
+        elif User.objects.filter(email = email).exists():
+            messages.info(request, "Email exists")
+            return redirect("/signup")
+        elif pno.objects.filter(number = number).exists():
+            messages.info(request, "Phone number already registered")
+            return redirect("/signup")
+        elif password != repass:
+            messages.info(request, "Passwords do not match")
+            return redirect("/signup")
+
+        user = User(username = username, password = password, email = email)
+        user.set_password(password)
+        user.save()
+        number = pno(user = user, number = number)
+        number.save()
+        messages.info(request, "Signup successful")
+        return redirect("/login")
+    else:
+        return render(request, "signup.html")
+    
+def logout(request):
+    auth.logout(request)
+    return redirect("/login")
